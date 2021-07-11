348adaf69951ae3ef9be176dbd4eaebdaf9c23f1
MaxJara <maxmata77@gmail.com>
2021-07-11 16:16:55 -0500

    Se agrego el proyecto

---
+++ accenture-market/src/main/java/com/accenture/market/AccentureMarketApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccentureMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccentureMarketApplication.class, args);
+	}
+
+}
---
+++ accenture-market/src/test/java/com/accenture/market/AccentureMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccentureMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
